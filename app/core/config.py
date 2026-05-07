894e6c029ac6b2cf0616014c7f3923e019bf98da
MARVRK <155836501+MARVRK@users.noreply.github.com>
2026-05-07 17:30:52 +0200

    add project configuration and update dependencies

---
+++ app/core/config.py
@@ -0,0 +1,9 @@
+
+CITIES = [
+    {"name": "Warsaw", "lat": 52.2298, "lon": 21.0118},
+    {"name": "Gdansk", "lat": 54.3520, "lon": 18.6466},
+    {"name": "Berlin", "lat": 52.5200, "lon": 13.4050},
+    {"name": "Krakow", "lat": 50.0647, "lon": 19.9450},
+    {"name": "Nurnberg", "lat": 49.4521, "lon": 11.0767},
+    {"name": "Munich", "lat": 48.1351, "lon": 11.5820},
+]
